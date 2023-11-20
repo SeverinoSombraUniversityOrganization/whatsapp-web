fed0c1ac30015cc8d2cad1236484c16c36bd293e
Deivid Hugo <deividhugoof@gmail.com>
2023-11-20 15:20:07 -0300

    Init ClientWebhook

---
+++ api-provider/controllers/clientWebhookController.js
@@ -0,0 +1,24 @@
+const clientWebhookConfig = {};
+
+const registerClientWebhook = (req, res) => {
+  const { url, events } = req.body;
+
+  clientWebhookConfig[url] = { events };
+
+  console.log(`Client Webhook registered for ${url} with events ${events}`);
+  res.json({ message: 'Client Webhook successfully registered' });
+};
+
+module.exports = {
+  registerClientWebhook,
+};
+
+
+
+
+
+
+
+
+
+
